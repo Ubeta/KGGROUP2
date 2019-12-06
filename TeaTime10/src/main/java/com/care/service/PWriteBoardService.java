812c2c67e184b8557db6c06bed053fa1ff829348
msdn <msdn@MONKI>
2019-12-06 18:37:06 +0900

    Commit to Pull 2 - John (12/6)

--- TeaTime10/src/main/java/com/care/service/PWriteBoardService.java
+++ TeaTime10/src/main/java/com/care/service/PWriteBoardService.java
@@ -1,10 +1,15 @@
 package com.care.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.care.modelDAO.ModelDAO;
 import com.care.modelDTO.PostDTO;
@@ -18,6 +23,26 @@ public class PWriteBoardService implements IService{
 	public void execute(Model model) {
 		Map<String, Object> map = model.asMap();
 		PostDTO pdto = (PostDTO)map.get("write_board");
+		
+		MultipartHttpServletRequest request = (MultipartHttpServletRequest) map.get("request");
+		System.out.println("File : " + request.getParameter("postImage"));
+		
+		MultipartFile multipartFile = request.getFile("postImage");
+		String originalFileName = multipartFile.getOriginalFilename();
+		System.out.println("MRegisterService Picture originalName: " + originalFileName);
+		String storedFileName = "null";
+		if (originalFileName != "") {
+			storedFileName = System.currentTimeMillis() + originalFileName;
+		}
+		String filePath = "C:\\TeaTime\\";
+		
+		pdto.setP_img(storedFileName);
+		
+		try {
+			Files.copy(multipartFile.getInputStream(), Paths.get(filePath, storedFileName));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 		mdao.write_board(pdto);
 	}
